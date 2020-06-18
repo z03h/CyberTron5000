6c346e93936271da34ff1e2dfb4f44d136d1369a
niztg <65248634+niztg@users.noreply.github.com>
2020-06-18 13:32:37 -0400

    A

--- cogs/Info.py
+++ cogs/Info.py
@@ -27,58 +27,62 @@ class Info(commands.Cog):
         list_of_cogs = []
         walk_commands = []
         final_walk_command_list = []
-        for cog in self.client.cogs:
-            list_of_cogs.append(cog)
-        if command:
-            cmd = self.client.get_command(command)
-        else:
-            cmd = None
-        if not command:
-            embed = discord.Embed(colour=colour, title=f"{self.client.user.name} Help",
-                                  description=f"You can do `{pre}help [category]` for more info on a category.\nYou can also do `{pre}help [command]` for more info on a command.")
-            for cog_name, cog_object in self.client.cogs.items():
-                cmds = []
-                for cmd in cog_object.get_commands():
-                    if not cmd.hidden:
-                        cmds.append(cmd.name)
-                embed.add_field(name=f'≫ {cog_name}', value='\u200b' + " • ".join(sorted(cmds)), inline=False)
-                embed.set_footer(text=footer)
-            for wc in self.client.walk_commands():
-                walk_commands.append(wc)
-            for item in walk_commands:
-                if item not in final_walk_command_list:
-                    final_walk_command_list.append(item)
-            embed.add_field(name="≫ Uncategorized Commands", value=" • ".join(final_walk_command_list))
-            await ctx.send(embed=embed)
-        elif command in list_of_cogs:
-            cog_doc = self.client.cogs[command].__doc__ or " "
-            embed = discord.Embed(title=f"Commands in {command}", colour=colour, description=cog_doc)
-            for cmd in self.client.cogs[command].get_commands():
-                help_msg = cmd.help or "No help provided for this command"
-                embed.add_field(name=f"{cmd.name} {cmd.signature}", value=help_msg, inline=False)
-                embed.set_footer(text=footer)
-            await ctx.send(embed=embed)
-        elif command and cmd:
-            help_msg = cmd.help or "No help provided for this command"
-            parent = cmd.full_parent_name
-            if len(cmd.aliases) > 0:
-                aliases = '•'.join(cmd.aliases)
-                cmd_alias_format = f'[{cmd.name}•{aliases}]'
-                if parent:
-                    cmd_alias_format = f'{parent} {cmd_alias_format}'
-                alias = cmd_alias_format
+        try:
+            for cog in self.client.cogs:
+                list_of_cogs.append(cog)
+            if command:
+                cmd = self.client.get_command(command)
             else:
-                alias = cmd.name if not parent else f'{parent} {cmd.name}'
-            embed = discord.Embed(title=f"{pre}{alias} {cmd.signature}", description=help_msg, colour=colour)
-            embed.set_footer(text=footer + " • ↠ indicates subcommands")
-            if isinstance(cmd, commands.Group):
-                for sub_cmd in cmd.commands:
-                    schm = sub_cmd.help or "No help provided for this command"
-                    embed.add_field(name=f"↠ {cmd.name} {sub_cmd.name} {sub_cmd.signature}", value=schm, inline=False)
-            await ctx.send(embed=embed)
-
-        else:
-            await ctx.send(f"Command/Cog `{command}` not found!")
+                cmd = None
+            if not command:
+                embed = discord.Embed(colour=colour, title=f"{self.client.user.name} Help",
+                                      description=f"You can do `{pre}help [category]` for more info on a category.\nYou can also do `{pre}help [command]` for more info on a command.")
+                for cog_name, cog_object in self.client.cogs.items():
+                    cmds = []
+                    for cmd in cog_object.get_commands():
+                        if not cmd.hidden:
+                            cmds.append(cmd.name)
+                    embed.add_field(name=f'≫ {cog_name}', value='\u200b' + " • ".join(sorted(cmds)), inline=False)
+                    embed.set_footer(text=footer)
+                for wc in self.client.walk_commands():
+                    if not wc.cog_name and not wc.hidden:
+                        walk_commands.append(wc.name)
+                for item in walk_commands:
+                    if item not in final_walk_command_list:
+                        final_walk_command_list.append(item)
+                embed.add_field(name="≫ Uncategorized Commands", value=" • ".join(final_walk_command_list))
+                await ctx.send(embed=embed)
+            elif command in list_of_cogs:
+                cog_doc = self.client.cogs[command].__doc__ or " "
+                embed = discord.Embed(title=f"Commands in {command}", colour=colour, description=cog_doc)
+                for cmd in self.client.cogs[command].get_commands():
+                    help_msg = cmd.help or "No help provided for this command"
+                    embed.add_field(name=f"{cmd.name} {cmd.signature}", value=help_msg, inline=False)
+                    embed.set_footer(text=footer)
+                await ctx.send(embed=embed)
+            elif command and cmd:
+                help_msg = cmd.help or "No help provided for this command"
+                parent = cmd.full_parent_name
+                if len(cmd.aliases) > 0:
+                    aliases = '•'.join(cmd.aliases)
+                    cmd_alias_format = f'[{cmd.name}•{aliases}]'
+                    if parent:
+                        cmd_alias_format = f'{parent} {cmd_alias_format}'
+                    alias = cmd_alias_format
+                else:
+                    alias = cmd.name if not parent else f'{parent} {cmd.name}'
+                embed = discord.Embed(title=f"{pre}{alias} {cmd.signature}", description=help_msg, colour=colour)
+                embed.set_footer(text=footer + " • ↠ indicates subcommands")
+                if isinstance(cmd, commands.Group):
+                    for sub_cmd in cmd.commands:
+                        schm = sub_cmd.help or "No help provided for this command"
+                        embed.add_field(name=f"↠ {cmd.name} {sub_cmd.name} {sub_cmd.signature}", value=schm, inline=False)
+                await ctx.send(embed=embed)
+    
+            else:
+                await ctx.send(f"Command/Cog `{command}` not found!")
+        except Exception as er:
+            await ctx.send(er)
 
 def setup(client):
     client.add_cog(Info(client))
