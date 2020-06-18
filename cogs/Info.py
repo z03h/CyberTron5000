fec9730e89101f0741266e022cacd15ed4b42d4b
niztg <65248634+niztg@users.noreply.github.com>
2020-06-18 16:59:35 -0400

    a

--- cogs/Info.py
+++ cogs/Info.py
@@ -85,8 +85,8 @@ class Info(commands.Cog):
                     sub_cmds = []
                     for sub_cmd in cmd.commands:
                         schm = sub_cmd.help or "No help provided for this command"
-                        sub_cmds.append(f"↝ **{cmd.name} {sub_cmd.name}** • {schm}")
-                await ctx.send(embed=discord.Embed(title=f"{pre}{alias} {cmd.signature}", description=help_msg + "\n" + "\n".join(sub_cmds), colour=colour))
+                        sub_cmds.append(f"≫ **{cmd.name} {sub_cmd.name}** • {schm}")
+                await ctx.send(embed=discord.Embed(title=f"{pre}{alias} {cmd.signature}", description=help_msg + "\n" + "\n".join(sub_cmds), colour=colour).set_footer(text=f"{footer} • ≫ are subcommands"))
     
             else:
                 await ctx.send(f"Command/Cog `{command}` not found!")
