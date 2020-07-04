a20b5bc2d3d75847b1c83c9c5e5aa0839be474af
niztg <65248634+niztg@users.noreply.github.com>
2020-07-04 00:38:34 -0400

    ily daggy

--- cogs/image.py
+++ cogs/image.py
@@ -1,5 +1,15 @@
-import discord
 from discord.ext import commands
+import json
+import aiohttp
+import discord
+
+colour = 0x00dcff
+
+
+def dagpi():
+    with open("secrets.json", "r") as f:
+        res = json.load(f)
+    return res['dagpi_token']
 
 
 class Images(commands.Cog):
@@ -8,6 +18,128 @@ class Images(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.tick = ":GreenTick:707950252434653184"
+        self.daggy = 491174779278065689
+    
+    @commands.command()
+    async def wanted(self, ctx, *, member: discord.Member = None):
+        """
+        Wanted...
+        """
+        daggy = await self.client.fetch_user(self.daggy)
+        member = member or ctx.author
+        data = {'token': dagpi(), 'url': str(member.avatar_url_as(static_format='png'))}
+        async with aiohttp.ClientSession() as cs:
+            async with cs.post('https://dagpi.tk/api/wanted', headers=data) as r:
+                data = await r.json()
+        response = data['url']
+        embed = discord.Embed(color=colour)
+        embed.set_image(url=response)
+        embed.set_footer(text=f"Much thanks to {str(daggy)} for this amazing API!", icon_url=daggy.avatar_url)
+        await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def obama(self, ctx, *, member: discord.Member = None):
+        """
+        I'm just great.
+        """
+        daggy = await self.client.fetch_user(self.daggy)
+        member = member or ctx.author
+        data = {'token': dagpi(), 'url': str(member.avatar_url_as(static_format='png'))}
+        async with aiohttp.ClientSession() as cs:
+            async with cs.post('https://dagpi.tk/api/obamameme', headers=data) as r:
+                data = await r.json()
+        response = data['url']
+        embed = discord.Embed(color=colour)
+        embed.set_image(url=response)
+        embed.set_footer(text=f"Much thanks to {str(daggy)} for this amazing API!", icon_url=daggy.avatar_url)
+        await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def bad(self, ctx, *, member: discord.Member = None):
+        """
+        Bad boy! Bad boy!
+        """
+        daggy = await self.client.fetch_user(self.daggy)
+        member = member or ctx.author
+        data = {'token': dagpi(), 'url': str(member.avatar_url_as(static_format='png'))}
+        async with aiohttp.ClientSession() as cs:
+            async with cs.post('https://dagpi.tk/api/bad', headers=data) as r:
+                data = await r.json()
+        response = data['url']
+        embed = discord.Embed(color=colour)
+        embed.set_image(url=response)
+        embed.set_footer(text=f"Much thanks to {str(daggy)} for this amazing API!", icon_url=daggy.avatar_url)
+        await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def hitler(self, ctx, *, member: discord.Member = None):
+        """
+        What a monster
+        """
+        daggy = await self.client.fetch_user(self.daggy)
+        member = member or ctx.author
+        data = {'token': dagpi(), 'url': str(member.avatar_url_as(static_format='png'))}
+        async with aiohttp.ClientSession() as cs:
+            async with cs.post('https://dagpi.tk/api/hitler', headers=data) as r:
+                data = await r.json()
+        response = data['url']
+        embed = discord.Embed(color=colour)
+        embed.set_image(url=response)
+        embed.set_footer(text=f"Much thanks to {str(daggy)} for this amazing API!", icon_url=daggy.avatar_url)
+        await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def tweet(self, ctx, member: discord.Member, *, tweet: str):
+        """
+        Yeah i use twitter
+        """
+        daggy = await self.client.fetch_user(self.daggy)
+        member = member or ctx.author
+        data = {'token': dagpi(), 'url': str(member.avatar_url_as(static_format='png')), 'text': tweet,
+                'name': member.display_name}
+        async with aiohttp.ClientSession() as cs:
+            async with cs.post('https://dagpi.tk/api/tweet', headers=data) as r:
+                data = await r.json()
+        response = data['url']
+        embed = discord.Embed(color=0x00dcff)
+        embed.set_image(url=response)
+        embed.set_footer(text=f"Much thanks to {str(daggy)} for this amazing API!", icon_url=daggy.avatar_url)
+        await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def quote(self, ctx, member: discord.Member, *, quote: str):
+        """
+        'Stop believing internet quotes' - God
+        """
+        daggy = await self.client.fetch_user(self.daggy)
+        member = member or ctx.author
+        data = {'token': dagpi(), 'url': str(member.avatar_url_as(static_format='png')), 'text': quote,
+                'name': member.display_name}
+        async with aiohttp.ClientSession() as cs:
+            async with cs.post('https://dagpi.tk/api/quote', headers=data) as r:
+                data = await r.json()
+        response = data['url']
+        embed = discord.Embed(color=0x00dcff)
+        embed.set_image(url=response)
+        embed.set_footer(text=f"Much thanks to {str(daggy)} for this amazing API!", icon_url=daggy.avatar_url)
+        await ctx.send(embed=embed)
+    
+    @commands.command()
+    async def triggered(self, ctx, *, member: discord.Member = None):
+        """
+        Brrrr
+        """
+        daggy = await self.client.fetch_user(self.daggy)
+        member = member or ctx.author
+        data = {'token': dagpi(), 'url': str(member.avatar_url_as(static_format='png'))}
+        async with aiohttp.ClientSession() as cs:
+            async with cs.post('https://dagpi.tk/api/triggered', headers=data) as r:
+                data = await r.json()
+        response = data['url']
+        embed = discord.Embed(color=0x00dcff)
+        embed.set_image(url=response)
+        embed.set_footer(text=f"Much thanks to {str(daggy)} for this amazing API!", icon_url=daggy.avatar_url)
+        await ctx.send(embed=embed)
 
 
 def setup(client):
