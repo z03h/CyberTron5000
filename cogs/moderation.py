f882360423e996f5db93719412493b3f690cb525
niztg <65248634+niztg@users.noreply.github.com>
2020-07-04 20:41:25 -0400

    cleaned up moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -13,6 +13,7 @@ class Moderation(commands.Cog):
     
     def __init__(self, client):
         self.client = client
+        self.tick = ":GreenTick:707950252434653184"
     
     @commands.command(aliases=['clear'])
     @commands.has_permissions(manage_messages=True)
@@ -20,69 +21,27 @@ class Moderation(commands.Cog):
         """ Purges a given amount of messages with the default being 5 """
         await ctx.message.delete()
         await ctx.channel.purge(limit=int(amount))
-        
-        # Deletes the messages
-        
-        notif_msg = await ctx.send(f"{amount} messages have been cleared.")
-        
-        # sends a little indicator that the messages have been purged
-        
-        await notif_msg.delete(delay=3)
-        
-        # deletes the indicator
+        await ctx.send(f"{amount} messages have been cleared.", delete_after=3)
     
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
-        """Kicks member from guild"""
-        if not reason:
-            
-            # If reason parameter is blank
-            
-            no_reason = "`No reason specified`"
-            await member.kick(reason=no_reason)
-            
-            # Kicks member
-            
-            await ctx.message.add_reaction(emoji=":GreenTick:707950252434653184")
-            
-            # sends them a message
-            
-            await member.send(
-                f"Hello, you have been kicked from participating in _{ctx.author.guild.name}_"
-                f". Please see below your reason for removal: `{reason}`.")
-        else:
-            
-            # if reason parameter isn't blank
-            
-            await member.kick(reason=reason)
-            
-            # kicks member
-            
-            await ctx.message.add_reaction(emoji=":GreenTick:707950252434653184")
-            
-            # sends them a message
-            
-            await member.send(
-                f"Hello, you have been kicked from participating in _{ctx.author.guild.name}_"
-                f". Please see below your reason for removal: `{reason}`.")
+        """Kick a member from a guild."""
+        r = "No reason specified" if not reason else reason
+        await member.kick(reason=r)
+        await member.send(
+            f"Hello, you have been kicked from participating in {ctx.guild}. Please see your reason for removal: `{r}`")
+        await ctx.message.add_reaction(self.tick)
     
     @commands.command(help="Ban a member.")
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: discord.Member, *, reason=None):
-        if reason is None:
-            reason = "`No reason specified`"
-            await member.ban(reason=reason)
-            await ctx.message.add_reaction(emoji=":GreenTick:707950252434653184")
-            await member.send(
-                f"Hello, you have been banned from participating in _{ctx.guild.name}_"
-                f". Please see below your reason for removal: `{reason}`.")
-        else:
-            await member.ban(reason=reason)
-            await ctx.message.add_reaction(emoji=":GreenTick:707950252434653184")
-            await member.send(
-                f"Hello, you have been banned from participating in _{ctx.guild.name}_"
-                f". Please see below your reason for removal: `{reason}`.")
+        """Ban a member from a guild."""
+        r = "No reason specified" if not reason else reason
+        await member.ban(reason=r)
+        await member.send(
+            f"Hello, you have been kicked from participating in {ctx.guild}. Please see your reason for removal: `{r}`")
+        await ctx.message.add_reaction(self.tick)
     
     @commands.command(help="Unban a member.")
     @commands.has_permissions(ban_members=True)
@@ -106,9 +65,9 @@ class Moderation(commands.Cog):
         await ctx.message.delete()
         string = str("Vote Here!")
         e = await ctx.send(string, embed=embed)
-        for r in [':upvote:718895913342337036', ':downvote:718895842404335668']:
+        for r in ['⬆️', '⬇️']:
             await e.add_reaction(r)
-            
+    
     @vote.command(invoke_without_command=True)
     @commands.is_owner()
     async def ct5k(self, ctx, *, message):
@@ -128,11 +87,8 @@ class Moderation(commands.Cog):
                     invoke_without_command=True)
     @commands.has_permissions(administrator=True)
     async def user_nick(self, ctx, member: discord.Member, *, name):
-        try:
-            await ctx.guild.get_member(member.id).edit(nick=name)
-            await ctx.message.add_reaction(emoji=":GreenTick:707950252434653184")
-        except Exception as error:
-            await ctx.send(error)
+        await ctx.guild.get_member(member.id).edit(nick=name)
+        await ctx.message.add_reaction(emoji=":GreenTick:707950252434653184")
     
     @user_nick.command(invoke_without_command=True)
     @commands.has_permissions(administrator=True)
@@ -140,14 +96,12 @@ class Moderation(commands.Cog):
         """Change nickname back to default."""
         await ctx.guild.get_member(member.id).edit(nick=member.name)
         await ctx.message.add_reaction(emoji=":GreenTick:707950252434653184")
-        
+    
     @commands.command()
     @check_admin_or_owner()
     async def leave(self, ctx):
         """Makes bot leave server"""
-        leave = self.client.get_guild(ctx.guild.id)
-        await leave.leave()
-
+        await self.client.get_guild(ctx.guild.id).leave()
 
 def setup(client):
     client.add_cog(Moderation(client))
