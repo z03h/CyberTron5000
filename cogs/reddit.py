b661c61472df8defce3f8648f914643c3829cd54
niztg <65248634+niztg@users.noreply.github.com>
2020-07-07 22:20:35 -0400

    PRAW extermination phase 5.25

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -26,17 +26,58 @@ password = secrets()['password']
 user_agent = secrets()['user_agent']
 
 
+async def get_reddit_user__data(data, scope: int):
+    """
+    Used for the RedditUser class
+    :param data:
+    :param scope:
+    :return:
+    """
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get(f"https://reddit.com/user/{data}/about/.json") as r:
+            resp = await r.json()
+        async with cs.get(f"https://reddit.com/user/{data}/trophies/.json") as r2:
+            resp2 = await r2.json()
+        async with cs.get(f"https://reddit.com/user/{data}/moderated_subreddits/.json") as r3:
+            resp3 = await r3.json()
+        t = (resp['data'], resp2['data'], resp3['data'])
+        tl = (r, r2, r3)
+        if scope < 1 or scope > 3:
+            return "Incorrect choice. Please choose between 1 and 3"
+        item = (t[scope - 1], tl[scope - 1])
+        if item[1].status != 200:
+            return f"something went wrong. Code: {item[1].status}"
+        else:
+            return item[0]
+
+
+class RedditUser:
+    """
+    Simple Reddit Info
+    """
+    
+    def __init__(self, general_data: dict):
+        self.user_data = general_data
+        self.name: str = general_data['name']
+        self.link_karma: int = general_data['link_karma']
+        self.comment_karma: int = general_data['comment_karma']
+        self.karma = self.link_karma + self.comment_karma
+        self.nsfw: bool = general_data['subreddit']['over_18']
+        self.description: bool = general_data['subreddit']['public_description']
+        self.icon_img = general_data['subreddit']['icon_img'].split("?")[0]
+        self.banner_img = general_data['subreddit']['banner_img'].split("?")[0]
+        self.title = general_data['subreddit']['title']
+    
+    def __repr__(self):
+        return f"RedditUser(name: {self.name}, karma: {self.karma:,})"
+
+
 class Reddit(commands.Cog):
     """Commands interacting with the Reddit API."""
     
     def __init__(self, client):
-        self.reddit = praw.Reddit(
-            client_id=client_id,
-            client_secret=client_secret,
-            username=username,
-            password=password,
-            user_agent=user_agent
-        )
+        self.reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, username=username,
+                                  password=password, user_agent=user_agent)
         self.client = client
         self.up = "<:upvote:718895913342337036>"
         self.share = "<:share:729813718086582402>"
@@ -46,37 +87,36 @@ class Reddit(commands.Cog):
     async def redditstats(self, ctx, user):
         trophies = []
         i = []
-        try:
-            async with ctx.typing():
-                async with aiohttp.ClientSession() as cs:
-                    async with cs.get(f"https://www.reddit.com/user/{user}/trophies/.json") as r:
-                        res = await r.json()
-                    for item in res['data']['trophies']:
-                        if str(item['data']['name']).lower() in emotes:
-                            trophies.append(emotes[str(item['data']['name']).lower()])
-                        else:
-                            trophies.append(" ")
-                    for t in trophies:
-                        if t not in i:
-                            i.append(t)
-                async with aiohttp.ClientSession() as cs:
-                    async with cs.get(f"https://www.reddit.com/user/{user}/about/.json") as r:
-                        k = await r.json()
-                embed = discord.Embed(
-                    colour=reddit_colour, title=f"u/{k['data']['name']}", url=f"https://reddit.com/user/{user}",
-                    description=f"{k['data']['subreddit']['public_description']}\n\n<:karma:704158558547214426> **Karma** • **{k['data']['link_karma'] + k['data']['comment_karma']:,}**\n:link: **Link** • **{k['data']['link_karma']:,}**\n:speech_balloon: **Comment** • **{k['data']['comment_karma']:,}**\n**Trophies (Total {len(i)})**\n" + "".join(
-                        i)
-                ).set_author(name=k['data']['subreddit']['title'])
-                embed.set_footer(
-                    text="Account created on " + datetime.datetime.utcfromtimestamp(k['data']['created_utc']).strftime(
-                        "%B %d, %Y"))
-                icon = k['data']['icon_img']
-                icon = icon.split("?")[0]
-                embed.set_thumbnail(url=icon)
+        async with ctx.typing():
+            async with aiohttp.ClientSession() as cs:
+                async with cs.get(f"https://www.reddit.com/user/{user}/trophies/.json") as r:
+                    res = await r.json()
+                for item in res['data']['trophies']:
+                    if str(item['data']['name']).lower() in emotes:
+                        trophies.append(emotes[str(item['data']['name']).lower()])
+                    else:
+                        trophies.append(" ")
+                for t in trophies:
+                    if t not in i:
+                        i.append(t)
+            async with aiohttp.ClientSession() as cs:
+                async with cs.get(f"https://www.reddit.com/user/{user}/about/.json") as re:
+                    k = await re.json()
+            embed = discord.Embed(
+                colour=reddit_colour, title=f"u/{k['data']['name']}", url=f"https://reddit.com/user/{user}",
+                description=f"{k['data']['subreddit']['public_description']}\n\n<:karma:704158558547214426> **Karma** • **{k['data']['link_karma'] + k['data']['comment_karma']:,}**\n:link: **Link** • **{k['data']['link_karma']:,}**\n:speech_balloon: **Comment** • **{k['data']['comment_karma']:,}**\n**Trophies (Total {len(i)})**\n" + "".join(
+                    i)
+            ).set_author(name=k['data']['subreddit']['title'])
+            embed.set_footer(
+                text="Account created on " + datetime.datetime.utcfromtimestamp(k['data']['created_utc']).strftime(
+                    "%B %d, %Y"))
+            icon = k['data']['icon_img']
+            icon = icon.split("?")[0]
+            embed.set_thumbnail(url=icon)
+            if r.status == 404:
+                await ctx.send('Redditor not found.')
+            else:
                 await ctx.send(embed=embed)
-        except Exception as error:
-            await ctx.send("Redditor not found.")
-            await ctx.send(error)
     
     @commands.command(aliases=['m'], help="Shows you a meme from some of reddit's dankest places (and r/memes)")
     async def meme(self, ctx):
@@ -138,7 +178,11 @@ class Reddit(commands.Cog):
                     mods = [i for i in data['children'] if str(i['name']).lower() == str(mod).lower()]
                     timestamp = [i['date'] for i in mods]
                     perms = [p['mod_permissions'] for p in mods]
-                    this = [f"{i['name']} | {__import__('html').unescape(i['author_flair_text'])}" for i in mods]
+                    if not [i['author_flair_text'] for i in mods][0]:
+                        char = "\u200b"
+                    else:
+                        char = f" | {__import__('html').unescape([i['author_flair_text'] for i in mods][0])}"
+                    this = [f"{i['name']}{char}" for i in mods]
                     embed = discord.Embed(colour=reddit_colour,
                                           description=f"Permissions: {f', '.join(f'**{p.capitalize()}**' for p in perms[0])}\nAdded as Mod: **{datetime.datetime.utcfromtimestamp(timestamp[0]).strftime('%B %d, %Y')}**").set_author(
                         name=this[0])
@@ -301,6 +345,16 @@ class Reddit(commands.Cog):
                         f"<:warning:727013811571261540> **{ctx.author.name}**, NSFW Channel required!")
         except Exception as er:
             await ctx.send(er)
+    
+    @commands.command()
+    @checks.betasquad()
+    async def qq(self, ctx, name):
+        r = RedditUser(await get_reddit_user__data(name, 1))
+        await ctx.send(f"**{r.name}**\n")
+        await ctx.send(r)
+        await ctx.send(r.icon_img)
+        await ctx.send(r.banner_img)
+        await ctx.send(r.nsfw)
 
 
 def setup(client):
