5590ffc50e60aba4593650507dcac32c8936266a
z0 <earkz0@gmail.com>
2020-07-28 22:59:47 -0700

    permission/hierarchy moderation checks. cleaner list comps, fewer db calls for tags

--- cogs/tags.py
+++ cogs/tags.py
@@ -32,21 +32,23 @@ class Tags(commands.Cog):
             source = paginator.IndexedListSource(embed=discord.Embed(color=self.client.colour, title="Tag Commands"),
                                                  data=final)
             return await paginator.CatchAllMenu(source=source).start(ctx)
-        tag = await self.client.pg_con.fetch("SELECT content FROM tags WHERE name = $1 AND guild_id = $2", name,
+        tag = await self.client.pg_con.fetch("SELECT content, uses FROM tags WHERE name = $1 AND guild_id = $2", name,
                                              str(ctx.guild.id))
         if not tag:
             await ctx.send("not found")
         else:
             await ctx.send(tag[0][0])
-            u = await self.client.pg_con.fetch("SELECT uses FROM tags WHERE name = $1 AND guild_id = $2", name,
-                                               str(ctx.guild.id))
-            if not u:
-                await self.client.pg_con.execute("INSERT INTO tags (uses) VALUES ($1)", 0)
-            else:
-                u = u[0][0] or 0
-                cu = u + 1
-                await self.client.pg_con.execute("UPDATE tags SET uses = $1 WHERE name = $2 AND guild_id = $3", cu,
-                                                 name, str(ctx.guild.id))
+            await self.client.pg_con.execute("UPDATE tags SET uses = $1 WHERE name = $2 AND guild_id = $3", tag[0][1]+1,
+                                             name, str(ctx.guild.id))
+            #u = await self.client.pg_con.fetch("SELECT uses FROM tags WHERE name = $1 AND guild_id = $2", name,
+            #                                   str(ctx.guild.id))
+            #if not u:
+            #    await self.client.pg_con.execute("INSERT INTO tags (uses) VALUES ($1)", 0)
+            #else:
+            #    u = u[0][0] or 0
+            #    cu = u + 1
+            #    await self.client.pg_con.execute("UPDATE tags SET uses = $1 WHERE name = $2 AND guild_id = $3", cu,
+            #                                    name, str(ctx.guild.id))
     
     @tag.command()
     async def list(self, ctx, member: discord.Member = None):
@@ -106,8 +108,8 @@ class Tags(commands.Cog):
                 msg = await self.client.wait_for('message', check=lambda m: m.author == ctx.author, timeout=15)
                 if msg.content.lower().startswith("y"):
                     await self.client.pg_con.execute(
-                        "INSERT INTO tags (user_id, guild_id, name, content) VALUES ($1, $2, $3, $4)", user_id,
-                        guild_id, name.strip(), message.content.strip()
+                        "INSERT INTO tags (user_id, guild_id, name, content, uses) VALUES ($1, $2, $3, $4, $5)", user_id,
+                        guild_id, name.strip(), message.content.strip(), 0
                     )
                     await ctx.send(f"Success! Tag `{name}` created!")
                 else:
@@ -177,23 +179,24 @@ class Tags(commands.Cog):
         if not tag:
             return await ctx.send("That tag was not found for this guild!")
         this = await self.client.pg_con.fetch("SELECT name, uses FROM tags WHERE guild_id = $1", str(ctx.guild.id))
-        uses = [0 if v[1] is None else v[1] for v in this]
-        names = [v[0] for v in this]
-        f = []
+        f = [(v[0], v[1] or 0) for v in this]
+        #uses = [v[1] or 0 for v in this]
+        #names = [v[0] for v in this]
+        #f = []
         tup = (tag[0][2], tag[0][4] or 0)
-        for x, y in zip(names, uses):
-            f.append((x, y))
+        #for x, y in zip(names, uses):
+        #    f.append((x, y))
         s = sorted(f, key=lambda x: x[1])[::-1]
         rank = s.index(tup) + 1
         embed = discord.Embed(title=tag[0][2], colour=self.client.colour)
-        owner = await self.client.fetch_user(int(tag[0][0]))
+        owner = self.client.get_user(int(tag[0][0])) or await self.client.fetch_user(int(tag[0][0]))
         embed.set_author(name=str(owner), icon_url=owner.avatar_url)
         embed.description = f'\nUses: **{tag[0][4] or 0}**'
         embed.description += f'\nRank: **{rank}**'
         embed.description += f'\nCreated Index: **{f.index(tup) + 1}**'
         await ctx.send(embed=embed)
     
-    async def get_user_badges(self, ctx, member: discord.Member = None):
+    async def get_user_badges(self, ctx, member: discord.Member):
         num_tags = await self.client.pg_con.fetch("SELECT * FROM tags WHERE user_id = $1 AND guild_id = $2",
                                                   str(member.id), str(ctx.guild.id))
         if not num_tags:
@@ -208,8 +211,9 @@ class Tags(commands.Cog):
             et = engineer_bagdes[:2]
         else:
             et = engineer_bagdes
-        uses = await self.client.pg_con.fetch("SELECT uses FROM tags WHERE user_id = $1 AND guild_id = $2",
-                                              str(member.id), str(ctx.guild.id))
+        uses = [(r.get('uses') for r in num_tags)]
+        #uses = await self.client.pg_con.fetch("SELECT uses FROM tags WHERE user_id = $1 AND guild_id = $2",
+        #                                      str(member.id), str(ctx.guild.id))
         il = sorted([0 if v[0] is None else v[0] for v in uses])[::-1][0]
         if il < 10:
             pt = []
@@ -221,20 +225,14 @@ class Tags(commands.Cog):
             pt = popular_badges[:3]
         else:
             pt = popular_badges
-        this = await self.client.pg_con.fetch("SELECT name, uses FROM tags WHERE guild_id = $1 and user_id = $2",
-                                              str(ctx.guild.id), str(member.id))
+        this = [(r.get('user_id'), r.get('uses')) for r in num_tags]
+        #this = await self.client.pg_con.fetch("SELECT name, uses FROM tags WHERE guild_id = $1 and user_id = $2",
+        #                                      str(ctx.guild.id), str(member.id))
         guild_tags = await self.client.pg_con.fetch("SELECT name, uses FROM tags WHERE guild_id = $1",
                                                     str(ctx.guild.id))
-        uses = [0 if v[1] is None else v[1] for v in this]
-        names = [v[0] for v in this]
-        g_uses = [0 if x[1] is None else x[1] for x in guild_tags]
-        g_names = [x[0] for x in guild_tags]
-        a = []
-        f = []
-        for w, z in zip(g_names, g_uses):
-            a.append((w, z))
-        for x, y in zip(names, uses):
-            f.append((x, y))
+        f = [(v[0], v[1] or 0) for v in this]
+        a = [(x[0], x[1] or 0) for x in guild_tags]
+
         s = sorted(f, key=lambda x: x[1])[::-1]
         n = sorted(a, key=lambda y: y[1])[::-1]
         nf = n.index(s[0]) + 1
@@ -280,6 +278,7 @@ class Tags(commands.Cog):
     
     @tag.command(aliases=['ui', 'user'])
     async def userinfo(self, ctx, *, member: discord.Member = None):
+        member = member or ctx.author
         user_id = str(member.id)
         embed = discord.Embed(colour=self.client.colour).set_author(name=f'{member}', icon_url=member.avatar_url)
 
